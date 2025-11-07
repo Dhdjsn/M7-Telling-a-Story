d3400b38665915786cff367f7621d5d4edfd44d9
Dhdjsn <jarodriguez3@s.sfusd.edu>
2025-11-07 15:00:01 -0800

    M7L12 Done

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -6,18 +6,26 @@ var expressions := {
 	"sad": preload("res://assets/emotion_sad.png"),
 }
 
+var bodies := {
+	"sophia": preload("res://assets/sophia.png"),
+	"pink": preload("res://assets/pink.png")
+}
+
 var dialogue_items: Array[Dictionary] = [
 	{
 		"expression": expressions["regular"],
 		"text": "I love my cat",
+		"character": bodies["sophia"],
 	},
 	{
 		"expression": expressions["happy"],
 		"text": "He's pretty cool",
+		"character": bodies["pink"],
 	},
 	{
 		"expression": expressions["sad"],
 		"text": "He does act like a little demon sometimes",
+		"character": bodies["sophia"],
 	},
 ]
 
@@ -38,9 +46,10 @@ func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item["text"]
 	expression.texture = current_item["expression"]
+	body.texture = current_item["character"]
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration := 1.2
+	var text_appearing_duration: float = current_item["text"]. length() / 30.0
 	tween.tween_property(rich_text_label,"visible_ratio",1.0,text_appearing_duration)
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
